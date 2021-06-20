b445756db85808fd67fe4a8098284a8f696a62ec
Alexandr Golobokov <bullet93639@mail.ru>
2021-06-20 17:43:08 +0300

    Resolve task number 2, and write tests

---
+++ homework1/other_tasks/tasks/task02.py
@@ -0,0 +1,30 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+    please write function "check_fib", which accepts a Sequence of integers, and
+    returns if the given sequence is a Fibonacci sequence
+
+We guarantee, that the given sequence contain >= 0 integers inside.
+
+"""
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    result_of_check = 0
+    local_list = []
+    for x in data:
+        local_list.append(x)
+    list_len = len(local_list)
+
+    if list_len > 2:
+        if local_list[0] == 0 or local_list[0] == 1:
+            for i in range(0, list_len-2):
+                if local_list[i+2] == local_list[i] + local_list[i+1]:
+                    result_of_check = 1
+                else:
+                    result_of_check = 0
+                    break
+        else:
+            result_of_check = 0
+
+    return bool(result_of_check)
---
+++ homework1/other_tasks/tests/test_task02.py
@@ -0,0 +1,39 @@
+import pytest
+
+from homework1.other_tasks.tasks.task02 import check_fibonacci
+
+
+def test_positive_case_when_start_with_0():
+    """"""
+    assert check_fibonacci([0, 1, 1, 2, 3])
+
+
+def test_positive_case_when_start_with_1():
+    """"""
+    assert check_fibonacci([1, 1, 2, 3, 5, 8, 13, 21])
+
+
+def test_negative_case_no_numbers():
+    """"""
+    assert not check_fibonacci([])
+
+
+def test_negative_case_when_start_with_0_var_0():
+    """"""
+    assert not check_fibonacci([0, 1, 1, 2, 3, 4])
+
+
+def test_negative_case_when_start_with_1_var_0():
+    """"""
+    assert not check_fibonacci([1, 1, 2, 3, 5, 8, 12, 20, 21])
+
+
+def test_negative_case_when_start_with_0_var_1():
+    """"""
+    assert not check_fibonacci([0, 1])
+    
+
+def test_negative_case_when_start_with_1_var_1():
+    """"""
+    assert not check_fibonacci([1, 1])
+
