43002634bd8094a89f4d04a58052aca4f5b60897
Alexandr Golobokov <bullet93639@mail.ru>
2021-06-22 00:16:40 +0300

    Resolve task 05, and write tests

---
+++ homework1/other_tasks/tasks/task05.py
@@ -0,0 +1,29 @@
+"""
+Given a list of integers numbers "nums".
+
+You need to find a sub-array with length less equal to "k", with maximal sum.
+
+The written function should return the sum of this sub-array.
+
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+from typing import List
+
+
+def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+    max_subarray = 0
+    copy_k = k
+    if k == 0:
+        ...  # do nothing
+    elif k > 0:
+        while copy_k != 0:
+            for i in range(0, len(nums)-copy_k + 1):
+                summ = 0
+                for j in range(0, copy_k):
+                    summ += nums[j + i]
+                    if max_subarray < summ:
+                        max_subarray = summ
+            copy_k = copy_k - 1
+    return max_subarray
---
+++ homework1/other_tasks/tests/test_task05.py
@@ -0,0 +1,25 @@
+import pytest
+
+from homework1.other_tasks.tasks.task05 import find_maximal_subarray_sum
+
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+
+def test_positive_case_find_maximal_subarray_sum_v1():
+    assert find_maximal_subarray_sum(nums, k) == 16
+
+
+n = 1
+
+
+def test_positive_case_find_maximal_subarray_sum_v2():
+    assert find_maximal_subarray_sum(nums, n) == 7
+
+
+j = 5
+
+
+def test_positive_case_find_maximal_subarray_sum_v3():
+    assert find_maximal_subarray_sum(nums, j) == 21
