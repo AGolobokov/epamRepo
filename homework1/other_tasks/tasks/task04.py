6d351b3c2631642e04382291e753b9ca9d6473f7
Alexandr Golobokov <bullet93639@mail.ru>
2021-06-27 18:21:11 +0300

    Rewrote task 4 with subject to comments, now o(3(n^2))

--- homework1/other_tasks/tasks/task04.py
+++ homework1/other_tasks/tasks/task04.py
@@ -11,10 +11,19 @@ from typing import List
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     result = 0
+    list_sum_1 = list()
     for i in a:
         for j in b:
-            for k in c:
-                for l in d:
-                    if i + j + k + l == 0:
-                        result = result + 1
+            list_sum_1.append(i+j)
+
+    list_sum_2 = list()
+    for k in c:
+        for l in d:
+            list_sum_2.append(k+l)
+
+    for i in list_sum_1:
+        for j in list_sum_2:
+            if i + j == 0:
+                result += 1
     return result
+
