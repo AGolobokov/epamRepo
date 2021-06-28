254aa8e4fd68fcfd4136479b08f9ee9646062bdf
Alexandr Golobokov <bullet93639@mail.ru>
2021-06-28 22:24:05 +0300

    Reformat test for task 4

--- homework2/tests/test_hw4.py
+++ homework2/tests/test_hw4.py
@@ -2,18 +2,19 @@ import pytest
 
 from homework2.tasks.hw4 import cache
 
+DATA_1 = 100, 200
+DATA_2 = 10, 20
 
-def func(a, b):
-    return (a ** b) ** 2
-
-
-cache_func = cache(func)
-
-some = 100, 200
-
-val_1 = cache_func(*some)
-val_2 = cache_func(*some)
 
 def test_positive_cache():
     """Testing that objects is equal"""
-    assert val_1 is val_2
+    def func(a, b):
+        return (a ** b) ** 2
+
+    cache_func = cache(func)
+
+    val_1 = cache_func(*DATA_1)
+    val_2 = cache_func(*DATA_1)
+    val_3 = cache_func(*DATA_2)
+    val_4 = cache_func(*DATA_2)
+    assert val_1 is val_2 and val_3 is val_4
