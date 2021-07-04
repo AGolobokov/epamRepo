77b79a2428ef2686318bcc8c73513f18209bb472
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-05 01:27:53 +0300

    Done task 3 with tests

---
+++ homework3/tests/test_task03.py
@@ -0,0 +1,28 @@
+import pytest
+
+from homework3.tasks.task03 import make_filter
+
+
+DATA_FOR_TEST = [
+     {
+         "name": "Bill",
+         "last_name": "Gilbert",
+         "occupation": "was here",
+         "type": "person",
+     },
+     {
+         "is_dead": True,
+         "kind": "parrot",
+         "type": "bird",
+         "name": "polly"
+     }
+]
+
+
+@pytest.mark.parametrize("arg, expected", [
+    ({'name':'polly', 'type':'bird'},[DATA_FOR_TEST[1]]),
+    ({'name':'Bill', 'type':'bird'}, []),
+    ({'name':'Bill', 'type':'person'},[DATA_FOR_TEST[0]]),
+    ])
+def test_positive_make_filter(arg, expected):
+    assert make_filter(**arg).apply(DATA_FOR_TEST) == expected
