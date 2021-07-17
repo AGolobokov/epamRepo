8ad2eac486b7992073bf09714b7628e6f0200d24
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-17 23:44:29 +0300

    Fix task 2 and wrote new test

--- homework3/tasks/task01.py
+++ homework3/tasks/task01.py
@@ -1,32 +1,23 @@
-valuee = 0
 
 
 def cache(times):
     cache_list = list()
-    print(cache_list)
-    print("times ==", times)
 
     def cache_decorator(function):
         def cache_wrapper(*args):
+            value = 0
             if not cache_list:
-                global valuee
-                valuee = function(*args)
-                # print("id val ==", id(valuee))
-                if valuee not in cache_list:
-                    for j in range(0, times + 1):
-                        cache_list.append(valuee)
-                # print(cache_list)
-                a = cache_list[0]
-                print(a)
-                cache_list.remove(valuee)
-                return a
+                value = function(*args)
+                if value not in cache_list:
+                    for i in range(0, times + 1):
+                        cache_list.append(value)
+                # print(value)
+                cache_list.remove(value)
+                return value
             else:
-                # print(cache_list)
-                b = cache_list[0]
-                print(b)
-                cache_list.remove(valuee)
-                return b
-
+                temp_value = cache_list[0]
+                # print(temp_value)
+                cache_list.remove(temp_value)
+                return temp_value
         return cache_wrapper
-
     return cache_decorator
--- homework3/tests/test_task01.py
+++ homework3/tests/test_task01.py
@@ -1,31 +1,28 @@
-from unittest.mock import MagicMock  # пока не доконца раскурил
-
-
+import pytest
 from homework3.tasks.task01 import cache
 
 
-def func(a, b):
+def func_example(a, b):
     return (a ** b) ** 2
 
 
-# мне кажется я сделал какую то херню :/
-cache_func = cache(times=2)(func)
+cache_func_example = cache(times=2)(func_example)
 
-VAL_1 = cache_func(2, 1)
-VAL_2 = cache_func(2, 2)
-VAL_3 = cache_func(2, 3)
-VAL_4 = cache_func(2, 4)
-VAL_5 = cache_func(2, 5)
-VAL_6 = cache_func(2, 6)
+ARGS1, ARGS2 = 200, 100
 
 
 def test_positive_cache_1():
-    assert VAL_1 is VAL_1
+    result_of_call = list()
+    for i in range(0, 6):
+        result_of_call.append(cache_func_example(ARGS1, ARGS2))
+    assert result_of_call[0] is result_of_call[1] is result_of_call[2]
+    assert result_of_call[3] is result_of_call[4] is result_of_call[5]
+    assert result_of_call[0] is not result_of_call[4]
 
 
-def test_positive_cache_2():
-    assert VAL_1 is VAL_3
 
 
-def test_positive_cache_3():
-    assert VAL_3 is not VAL_4 and VAL_4 is VAL_5
+
+
+
+
