933c558b369e45a7db7a9929b31a43975cfd14c5
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-19 01:08:55 +0300

    Done task 4 with tests

--- homework4/tasks/task_4_doctest.py
+++ homework4/tasks/task_4_doctest.py
@@ -20,7 +20,23 @@ assert fizzbuzz(5) == ["1", "2", "fizz", "4", "buzz"]
 ** Энциклопедия профессора Фортрана page 14, 15, "Робот Фортран, чисть картошку!"
 """
 from typing import List
+import doctest
 
 
 def fizzbuzz(n: int) -> List[str]:
-    pass
+    """
+    Get the int value and return sequence.
+    Example:
+    >>> fizzbuzz(5)
+    [1, 2, 'fuzz', 4, 'buzz']
+    >>> fizzbuzz(15)
+    [1, 2, 'fuzz', 4, 'buzz', 'fuzz', 7, 8, 'fuzz', 'buzz', 11, 'fuzz', 13, 14, 'fuzz buzz']
+    """
+    result_list = ["fuzz buzz" if not i % 3 and not i % 5 else "fuzz" if not i % 3 else i if i % 5 else "buzz" for i in range(1, n + 1)]
+    return result_list
+
+
+if __name__ == '__main__':
+    fizzbuzz(1)
+    doctest.testmod(verbose=True)
+
