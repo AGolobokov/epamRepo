ac34df7a5943dd7f8fec440fcf02b27c38334131
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-19 01:52:08 +0300

    Done task 5

--- homework4/tasks/task_5_optional.py
+++ homework4/tasks/task_5_optional.py
@@ -12,14 +12,31 @@ Definition of done:
  - function has tests
 
 
->>> list(fizzbuzz(5))
+# >>> list(fizzbuzz(5))
 ["1", "2", "fizz", "4", "buzz"]
 
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** https://www.youtube.com/watch?v=NSzsYWckGd4
 """
-from typing import List, Generator
+from typing import List, Generator, Any
 
 
-def fizzbuzz(n: int) -> Generator[str]:
-    pass
+def generate_fuzz_buzz(n: int):
+    value = ""
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            value = "fuzz buzz"
+        elif i % 3 == 0:
+            value = "fuzz"
+        elif i % 5 == 0:
+            value = "buzz"
+        else:
+            value = i
+        yield str(value)
+
+
+def fizzbuzz(n: int) -> Generator[None, None, str]:
+    generate_fuzz_buzz(n)
+    return generate_fuzz_buzz(n)
+
+
