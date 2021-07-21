1af2d488062c65f9761e7fcb73f74d6a7c7c8118
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-22 01:59:47 +0300

    Done first task with tests

---
+++ homework6/tests/test_counter.py
@@ -0,0 +1,16 @@
+import pytest
+
+from homework6.tasks.counter import instances_counter
+
+
+def test_decorator_instances_counter():
+    @instances_counter
+    class AnyClass:
+        pass
+
+    example_any_class = AnyClass()
+
+    assert hasattr(example_any_class, 'counter')
+    assert hasattr(example_any_class, 'get_created_instances') and callable(example_any_class.get_created_instances)
+    assert hasattr(example_any_class, 'reset_instances_counter') and callable(example_any_class.reset_instances_counter)
+
