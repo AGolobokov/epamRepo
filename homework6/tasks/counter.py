06adf38358263867d01d8a588737a20a0803aa3b
Alexandr Golobokov <bullet93639@mail.ru>
2021-07-24 19:33:04 +0300

    Resolve  task 1

--- homework6/tasks/counter.py
+++ homework6/tasks/counter.py
@@ -13,20 +13,21 @@ reset_instances_counter - сбросить счетчик экземпляров
 def instances_counter(cls):
     """Some code"""
 
-    class NewCls:
+    class NewCls(cls):
 
         counter = 0
         setattr(cls, 'counter', counter)
 
         def __init__(self, *args, **kwargs):
+            super(type(cls), cls).__init__()
             cls.counter += 1
-            self._obj = cls(*args, **kwargs)
+
+        setattr(cls, '__init__', __init__)
 
         @staticmethod
         def get_created_instances(*args):
             return cls.counter
 
-        property(get_created_instances)
         setattr(cls, 'get_created_instances', get_created_instances)
 
         @staticmethod
@@ -35,19 +36,20 @@ def instances_counter(cls):
             cls.counter = 0
             return temp_value
 
-        property(reset_instances_counter)
         setattr(cls, 'reset_instances_counter', reset_instances_counter)
-    return NewCls
+
+    return cls
 
 
 @instances_counter
 class User:
-    pass
+    def __init__(self):
+        print("Hello User!")
 
 
 if __name__ == '__main__':
 
     User.get_created_instances()  # 0
     user, _, _ = User(), User(), User()
-    user.get_created_instances()   # 3
-    user.reset_instances_counter() # 3
+    user.get_created_instances()  # 3
+    user.reset_instances_counter()  # 3
--- homework6/tests/test_counter.py
+++ homework6/tests/test_counter.py
@@ -14,3 +14,4 @@ def test_decorator_instances_counter():
     assert hasattr(example_any_class, 'get_created_instances') and callable(example_any_class.get_created_instances)
     assert hasattr(example_any_class, 'reset_instances_counter') and callable(example_any_class.reset_instances_counter)
 
+
