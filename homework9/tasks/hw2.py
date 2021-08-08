e1daf4d854835701dcb6aac0da84de0f0ebd1f3d
Alexandr Golobokov <bullet93639@mail.ru>
2021-08-09 01:55:17 +0300

    Done task 2 with tests

--- homework9/tasks/hw2.py
+++ homework9/tasks/hw2.py
@@ -2,7 +2,47 @@
 Write a context manager, that suppresses passed exception.
 Do it both ways: as a class and as a generator.
 
->>> with supressor(IndexError):
+# >>> with supressor(IndexError):
 ...    [][2]
 
 """
+import contextlib
+
+
+class SupressorClass:
+    """
+    This class is context manager, that suppresses passed exception
+    """
+
+    def __init__(self, error):
+        self.error = error
+
+    def __enter__(self):
+        print('__enter__')
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print('__exit__')
+        return isinstance(exc_value, self.error)
+
+
+@contextlib.contextmanager
+def supressor_generator(error):
+    """
+     This generator is context manager, that suppresses passed exception
+    :param error: name of exception that we should suppress
+    :return:
+    """
+    try:
+        yield
+    except error:
+        print(f"Exception {error} was suppressed")
+
+
+if __name__ == "__main__":
+    with SupressorClass(AssertionError):
+        raise AssertionError
+
+    with supressor_generator(AssertionError):
+        raise AssertionError
+
+
