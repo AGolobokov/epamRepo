bdcdcea04c90c8ef95c101906644186b46bf0362
Alexandr Golobokov <bullet93639@mail.ru>
2021-08-17 03:17:00 +0300

    Done with multiprocessing

--- homework10/tasks/task_stonks_multi.py
+++ homework10/tasks/task_stonks_multi.py
@@ -3,36 +3,11 @@ import requests
 import datetime
 import operator
 import json
-import time
 
-from multiprocessing import Pool
-
-# get central bank valute data
-now = datetime.datetime.now()
-day = now.day
-year = now.year
-month = 0
-
-if now.month < 10:
-    month = '0'+str(now.month)
-
-cb_url = f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{month}/{year}"
-
-cb_data_text = requests.get(cb_url, timeout=5).text
-currencies_soup = BeautifulSoup(cb_data_text, "lxml")
-currencies = currencies_soup.find_all('valute')
-
-one_dollar_price = 0
-for elm in currencies:
-    if elm.find('charcode').text == 'USD':
-        one_dollar_price = elm.find('value').text
-        one_dollar_price = float(one_dollar_price.replace(",", "."))
-
-url = 'https://markets.businessinsider.com/index/components/s&p_500'
+from multiprocessing import Manager, Process
 
 
 class Company:
-
     name = str
     ticker = str
     price = float
@@ -43,7 +18,7 @@ class Company:
     def __init__(self, name, ticker, price, one_year_gain, pe_ratio, potential_52_week_profit):
         self.name = name
         self.ticker = ticker
-        self.price = round(one_dollar_price * float(price), 2)
+        self.price = round(73 * float(price), 2)
         self.one_year_gain = one_year_gain
         self.pe_ratio = pe_ratio
         self.potential_52_week_profit = potential_52_week_profit
@@ -55,29 +30,14 @@ class Company:
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
-company_list = list()
-
-html_text = requests.get(url, timeout=5).text
-title_page_content = BeautifulSoup(html_text, "html.parser")
-
-
-# get page numbers
-num_of_pages = title_page_content.select(".finando_paging a")
-
-page_num_list = list()
-for elm in num_of_pages:
-    page_num = BeautifulSoup(str(elm), 'html.parser').a.text
-    if str(page_num).isdigit():
-        page_num_list.append(int(page_num))
-
-
 # get data of company
 def walk_on_the_page(page_num):
-# for i in range(min(page_num_list), max(page_num_list) + 1):
+    company_list = list()
+    # for i in range(min(page_num_list), max(page_num_list) + 1):
     # get data from main page
-    print(f"page number {i}")
+    print(f"Processing page number {page_num}")
     company_name_dict = dict()
-    html_text_page = requests.get(f'https://markets.businessinsider.com/index/components/s&p_500?p={i}', timeout=5).text
+    html_text_page = requests.get(f'https://markets.businessinsider.com/index/components/s&p_500?p={page_num}',timeout=5).text
 
     content_from_page = BeautifulSoup(html_text_page, "html.parser")
     table_data = content_from_page.find('table', {'class': 'table table__layout--fixed'})
@@ -89,6 +49,7 @@ def walk_on_the_page(page_num):
         clean_year_profit = float(year_profit.replace("%", ""))
         link = data.find('td', class_='table__td table__td--big').a.get('href')
         company_name_dict[name.strip()] = [link.strip(), clean_year_profit]
+    print(company_name_dict)
 
     # get data from company page
     for key in company_name_dict:
@@ -114,62 +75,123 @@ def walk_on_the_page(page_num):
                 high_week = str(elm.text).strip().split()[0]
                 clean_high_week = high_week.replace(",", '')
 
-        potential_profit = round((float(clean_high_week) - float(clean_low_week))/float(clean_low_week)*100, 2)
-        new_company = Company(key, take_ticker,  price, company_name_dict[key][1], clean_pe_data, potential_profit)
+        potential_profit = round((float(clean_high_week) - float(clean_low_week)) / float(clean_low_week) * 100, 2)
+        new_company = Company(key, take_ticker, price, company_name_dict[key][1], clean_pe_data, potential_profit)
         company_list.append(new_company)
+    return company_list
 
 
-if __name__ == "main":
-    with Pool(48) as p:
-        answer = sum(p.map(walk_on_the_page, range(min(page_num_list), max(page_num_list) + 1)))
+def worker(n, any_list):
+    any_list.append(walk_on_the_page(n))
 
 
-company_list.sort(key=operator.attrgetter('price'))
-top_price_list = company_list[len(company_list)-10:]
-top_price_list.reverse()
+if __name__ == "__main__":
 
-with open('price.json', 'w') as outfile:
-    print("Top price")
-    counter = 0
-    for i in top_price_list:
-        print(counter, i)
-        counter += 1
-        json.dump(i.toJSON(), outfile)
+    t_start = datetime.datetime.now()
+    print(t_start)
+
+    # get central bank valute data
+    now = datetime.datetime.now()
+    day = now.day
+    year = now.year
+    month = 0
+
+    if now.month < 10:
+        month = '0' + str(now.month)
+
+    cb_url = f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{month}/{year}"
+
+    cb_data_text = requests.get(cb_url, timeout=5).text
+    currencies_soup = BeautifulSoup(cb_data_text, "lxml")
+    currencies = currencies_soup.find_all('valute')
+
+    one_dollar_price = 0
+    for elm in currencies:
+        if elm.find('charcode').text == 'USD':
+            one_dollar_price = elm.find('value').text
+            one_dollar_price = float(one_dollar_price.replace(",", "."))
+
+    url = 'https://markets.businessinsider.com/index/components/s&p_500'
+
+    company_list = list()
+
+    html_text = requests.get(url, timeout=5).text
+    title_page_content = BeautifulSoup(html_text, "html.parser")
+
+    # get page numbers
+    num_of_pages = title_page_content.select(".finando_paging a")
+
+    page_num_list = list()
+    for elm in num_of_pages:
+        page_num = BeautifulSoup(str(elm), 'html.parser').a.text
+        if str(page_num).isdigit():
+            page_num_list.append(int(page_num))
+
+    manager = Manager()
+    return_list = manager.list()
+    process_work = []
+
+    for number in range(1, 12):
+        proc = Process(target=worker, args=(number, return_list))
+        process_work.append(proc)
+        proc.start()
+
+    for proc in process_work:
+        proc.join()
+
+    company_list = [elm for subarray in return_list for elm in subarray]
+
+    company_list.sort(key=operator.attrgetter('price'))
+    top_price_list = company_list[len(company_list) - 10:]
+    top_price_list.reverse()
+
+    with open('price.json', 'w') as outfile:
+        print("Top price")
+        counter = 0
+        for i in top_price_list:
+            print(counter, i)
+            counter += 1
+            json.dump(i.toJSON(), outfile)
+
+    company_list.sort(key=operator.attrgetter('pe_ratio'))
+    top_pe_list = company_list[:10]
+    with open('top_pe_ratio.json', 'w') as outfile:
+        print("Top pe")
+        counter = 0
+        for i in top_pe_list:
+            print(counter, i)
+            counter += 1
+            json.dump(i.toJSON(), outfile)
+
+    company_list.sort(key=operator.attrgetter('one_year_gain'))
+    top_one_year_profit_list = company_list[len(company_list) - 10:]
+    top_one_year_profit_list.reverse()
+    with open('top_one_year_gain.json', 'w') as outfile:
+        print("Top one year gain")
+        counter = 0
+        for i in top_one_year_profit_list:
+            print(counter, i)
+            counter += 1
+            json.dump(i.toJSON(), outfile)
+
+    company_list.sort(key=operator.attrgetter('potential_52_week_profit'))
+    top_one_year_profit_list = company_list[len(company_list) - 10:]
+    top_one_year_profit_list.reverse()
+    with open('top_25_week_profit.json', 'w') as outfile:
+        print("Top 52 week deal profit")
+        counter = 0
+        for i in top_one_year_profit_list:
+            print(counter, i)
+            counter += 1
+            json.dump(i.toJSON(), outfile)
+
+    t_stop = datetime.datetime.now()
+    t_delta = t_start - t_stop
+
+    print("Time of execution = ", t_delta)
 
 
-company_list.sort(key=operator.attrgetter('pe_ratio'))
-top_pe_list = company_list[:10]
-with open('top_pe_ratio.json', 'w') as outfile:
-    print("Top pe")
-    counter = 0
-    for i in top_pe_list:
-        print(counter, i)
-        counter += 1
-        json.dump(i.toJSON(), outfile)
 
-
-company_list.sort(key=operator.attrgetter('one_year_gain'))
-top_one_year_profit_list = company_list[len(company_list)-10:]
-top_one_year_profit_list.reverse()
-with open('top_one_year_gain.json', 'w') as outfile:
-    print("Top one year gain")
-    counter = 0
-    for i in top_one_year_profit_list:
-        print(counter, i)
-        counter += 1
-        json.dump(i.toJSON(), outfile)
-
-
-company_list.sort(key=operator.attrgetter('potential_52_week_profit'))
-top_one_year_profit_list = company_list[len(company_list)-10:]
-top_one_year_profit_list.reverse()
-with open('top_25_week_profit.json', 'w') as outfile:
-    print("Top 52 week deal profit")
-    counter = 0
-    for i in top_one_year_profit_list:
-        print(counter, i)
-        counter += 1
-        json.dump(i.toJSON(), outfile)
 
 
 
